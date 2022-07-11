d09ca9efe67687ce5efb11ec6b6e9f3313feb9a7
Kenzhe <kenzhekozy@gmail.com>
2022-07-11 14:35:05 +0600

    1.0

--- src/main/java/com/example/workflow/PrepareToBattle.java
+++ src/main/java/com/example/workflow/PrepareToBattle.java
@@ -8,10 +8,24 @@ import org.springframework.stereotype.Component;
 public class PrepareToBattle implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        int warriors = (int) delegateExecution.getVariable("warriors");
+
         int enemyWarriors = (int) (Math.random() * 100);
         String battleStatus = "Undefined";
 
+        boolean isWin = false;
+
+        if (warriors > enemyWarriors) {
+            isWin = true;
+        } else {
+            isWin = false;
+        }
+
         delegateExecution.setVariable("enemyWarriors", enemyWarriors);
         delegateExecution.setVariable("battleStatus", battleStatus);
+        delegateExecution.setVariable("isWin", isWin);
+
+
     }
 }
