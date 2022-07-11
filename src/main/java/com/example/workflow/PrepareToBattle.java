33c66c497fcd9186ef6cd1c9abdef1bf20dbc4a1
Kenzhe <kenzhekozy@gmail.com>
2022-07-11 15:23:14 +0600

    1.1

--- src/main/java/com/example/workflow/PrepareToBattle.java
+++ src/main/java/com/example/workflow/PrepareToBattle.java
@@ -1,5 +1,6 @@
 package com.example.workflow;
 
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
@@ -11,20 +12,27 @@ public class PrepareToBattle implements JavaDelegate {
 
         int warriors = (int) delegateExecution.getVariable("warriors");
 
+        if (warriors < 0 || warriors > 100) {
+            throw new BpmnError("warriorsError");
+        }
+
         int enemyWarriors = (int) (Math.random() * 100);
         String battleStatus = "Undefined";
 
-        boolean isWin = false;
+        boolean isWin;
 
         if (warriors > enemyWarriors) {
             isWin = true;
+            battleStatus = "Victory";
         } else {
             isWin = false;
+            battleStatus = "Defeat";
         }
 
         delegateExecution.setVariable("enemyWarriors", enemyWarriors);
         delegateExecution.setVariable("battleStatus", battleStatus);
         delegateExecution.setVariable("isWin", isWin);
+        delegateExecution.setVariable("warriors", warriors);
 
 
     }
